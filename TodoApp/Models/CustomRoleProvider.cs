c0d821f44deaa724fb87e5ae9a5cb9d54f01fe0b
ShunjiYamanaka <syun.singernet@gmail.com>
2020-02-25 00:03:28 +0900

    MemberShipProviderを追加

---
+++ TodoApp/Models/CustomRoleProvider.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace TodoApp.Models
+{
+    public class CustomRoleProvider : RoleProvider
+    {
+        public override string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+            //指定されたユーザーのロールを配列で返す
+            if ("administrator".Equals(username))
+            {
+                return new string[] { "Administrators" };
+            }
+            return new string[] { "Users" };
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            //引数で与えたユーザが引数のロールに存在するか
+            if ("administrator".Equals(username) && "Administators".Equals(roleName)) 
+            {
+                return true;
+            }
+            if ("user".Equals(username) && "Users".Equals(roleName)) 
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
